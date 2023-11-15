c477f8c7b23b27dbfcca2706000fb6544852f481
unknown <jborelli95@gmail.com>
2023-11-15 09:57:36 -0300

    mini parche, arreglando un error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/navegacion/home-page/home-page.component';
 import { RegisterPageComponent } from './pages/users/register-page/register-page.component';
 import { LoginPageComponent } from './pages/users/login-page/login-page.component';
-import { EditPageComponent } from './pages/users/edit-page/edit-page.component';
 import { TestComponent } from './components/tests/test/test.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { GruposPageComponent } from './pages/navegacion/grupos-page/grupos-page.component';
@@ -48,10 +47,6 @@ const routes: Routes = [
   {
     path:"register",
     component:RegisterPageComponent
-  },
-  {
-    path:"editar/:id",
-    component:EditPageComponent
   },
   {
     path:"test",
